02bfe294818da34d86a0d62622bd4316111f3ef1
devendra kumar <devendrakumaraa@gmail.com>
2025-01-13 17:36:47 +0530

    undo the index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import cookieParser from "cookie-parser";
 connectDB()
 
 const app = express();
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true, }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
