559e39108f46f2bfa39ded2742269e8c56c78612
devendra kumar <devendrakumaraa@gmail.com>
2025-01-20 16:12:16 +0530

    Add Recommendations API

--- controller/rest.controller.js
+++ controller/rest.controller.js
@@ -72,5 +72,48 @@ const getSearchQuery = async (req, res) => {
     }
 }
 
+const getRecommended = async (req, res) => {
+    try{
+        const { type } = req.params;
+        const genres = {
+            movie: [28, 12, 16, 35, 80, 99, 18, 10751, 14, 36, 27, 10402, 9648, 10749, 878, 10770, 53, 10752, 37],
+            tv: [35, 16, 18, 99, 80, 10762, 10751, 10759, 9648, 10763, 10764, 10765, 10766, 10767, 10768, 37]
+          };
+          
+        const genreId = genres[type]?.[Math.floor(Math.random() * genres[type]?.length)] || null;
+          
 
- export { getTrending, getSearchQuery };
+        const url = `${process.env.TMDB_BASE_URL}discover/${type}?include_adult=false&include_video=true&language=en-US&page=1&sort_by=popularity.desc&with_genres=${genreId}`;;
+        const options = {
+            method: 'GET',
+            headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`
+            }
+        };
+
+        const response = await fetch(url, options);
+
+        const data = await response.json();
+        if (!response.ok) {
+            return res.status(400).json(new ApiError(false, "Failed to fetch search results"));
+        }
+        const bookmarks = await bookmarkModel.find({userId: req.user._id}).select("movieId");
+        const searchResults = data.results.map((movie) => {
+            const isTrue = bookmarks.some(value => (value.movieId === movie.id))
+            return {
+                ...movie,
+                backdrop_path: movie.backdrop_path ? `${process.env.TMDB_IMAGE_URL}${movie.backdrop_path}` : null,
+                poster_path: movie.poster_path ? `${process.env.TMDB_IMAGE_URL}${movie.poster_path}` : null,
+                isBookmark: isTrue
+            }})
+        
+        res.status(200).json(new ApiResponse(true, "recommended movies and tv series fetched successfully", searchResults));
+    }catch(error){
+        res.status(500).json(new ApiError(false, error.message));
+    
+    } 
+}
+
+
+ export { getTrending, getSearchQuery, getRecommended };
--- routes/rest.routes.js
+++ routes/rest.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import authMiddleware from "../middleware/auth.middleware.js";
-import { getTrending, getSearchQuery } from "../controller/rest.controller.js";
+import { getTrending, getSearchQuery, getRecommended } from "../controller/rest.controller.js";
 
 const routes = express.Router();
 
 
 routes.get("/trending/movies_&_tv_series", authMiddleware, getTrending);
 routes.get('/search/:type/:query/:page', authMiddleware, getSearchQuery);
+routes.get('/recommendations/:type', authMiddleware, getRecommended);
 
 
 export default routes
